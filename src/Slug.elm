200473203c4918b9e47b18ce1e1ade58add23245
Héctor Ramón Jiménez <hector0193@gmail.com>
2017-07-01 20:44:14 +0200

    Initial commit

---
+++ src/Slug.elm
@@ -0,0 +1,112 @@
+module Slug
+    exposing
+        ( Slug
+        , generate
+        , parse
+        , toString
+        )
+
+{-| Type-safe slugs for Elm
+
+
+# Definition
+
+@docs Slug
+
+
+# Constructors
+
+@docs generate, parse
+
+
+# Helpers
+
+@docs toString
+
+-}
+
+import Regex
+
+
+{-| Represents a slug
+-}
+type Slug
+    = Slug String
+
+
+{-| Generate a valid slug from a given text.
+
+If a valid slug can be generated it returns just the slug, otherwise nothing is
+returned.
+
+    generate "Some text here" == Just (Slug "some-text-here")
+    generate "--!@·==)/()" == Nothing
+
+-}
+generate : String -> Maybe Slug
+generate text =
+    let
+        words =
+            processWords text
+    in
+    if words == [] then
+        Nothing
+    else
+        Just (Slug (String.join "-" words))
+
+
+{-| Parse a slug from its string representation.
+
+It returns the slug if the input is a valid slug, otherwise it returns nothing.
+
+    parse "some-valid-slug" == Just (Slug "some-valid-slug")
+    parse "Not a valid slug" == Nothing
+    parse "another-invalid-slug-" == Nothing
+
+-}
+parse : String -> Maybe Slug
+parse slug =
+    case generate slug of
+        Just s ->
+            if slug == toString s then
+                Just s
+            else
+                Nothing
+
+        Nothing ->
+            Nothing
+
+
+{-| Returns the string represntation of a slug.
+
+    Maybe.map toString (generate "Some text") == Just "some-text"
+
+-}
+toString : Slug -> String
+toString (Slug s) =
+    s
+
+
+processWords : String -> List String
+processWords =
+    let
+        mapHelp c =
+            if isAlphanumeric c then
+                c
+            else
+                ' '
+    in
+    Regex.replace Regex.All simpleQuoteRegex (\_ -> " ")
+        >> String.toLower
+        >> String.map mapHelp
+        >> String.words
+
+
+isAlphanumeric : Char -> Bool
+isAlphanumeric c =
+    (c >= '0') && (c <= '9') || (c >= 'a') && (c <= 'z')
+
+
+simpleQuoteRegex : Regex.Regex
+simpleQuoteRegex =
+    Regex.regex "'"
---
+++ tests/Main.elm
@@ -0,0 +1,106 @@
+module Main exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, custom, int, list, string)
+import Random.Pcg as Random
+import Shrink
+import Slug exposing (Slug)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Slug"
+        [ describe "slug properties"
+            [ fuzz alphanumericWords "cannot be empty" <|
+                Slug.generate
+                    >> just (Slug.toString >> Expect.notEqual "")
+            , fuzz alphanumericWords "contains only dashes and alphanumeric characters" <|
+                Slug.generate
+                    >> just
+                        (Slug.toString
+                            >> String.toList
+                            >> List.all (\c -> isAlphanumeric c || c == '-')
+                            >> Expect.true "Expected slug to be alphanumeric"
+                        )
+            , fuzz alphanumericWords "does not begin with a dash" <|
+                Slug.generate
+                    >> just
+                        (Slug.toString
+                            >> String.left 1
+                            >> Expect.notEqual "-"
+                        )
+            , fuzz alphanumericWords "does not end with a dash" <|
+                Slug.generate
+                    >> just
+                        (Slug.toString
+                            >> String.right 1
+                            >> Expect.notEqual "-"
+                        )
+            , fuzz alphanumericWords "does not contain empty words between dashes" <|
+                Slug.generate
+                    >> just
+                        (Slug.toString
+                            >> String.split "-"
+                            >> List.any ((==) "")
+                            >> Expect.false "Expected no empty words between dashes"
+                        )
+            , fuzz alphanumericWords "does not contain upper case characters" <|
+                Slug.generate
+                    >> just
+                        (\slug ->
+                            Expect.equal
+                                (String.toLower (Slug.toString slug))
+                                (Slug.toString slug)
+                        )
+            ]
+        , describe "generate"
+            [ fuzz alphanumericWords "slug generation is idempotent" <|
+                \text ->
+                    Expect.equal
+                        (Slug.generate text)
+                        (Slug.generate text
+                            |> Maybe.map Slug.toString
+                            |> Maybe.andThen Slug.generate
+                        )
+            ]
+        ]
+
+
+just : (a -> Expect.Expectation) -> Maybe a -> Expect.Expectation
+just function aMaybe =
+    case aMaybe of
+        Just value ->
+            function value
+
+        Nothing ->
+            Expect.notEqual Nothing aMaybe
+
+
+alphanumericWords : Fuzzer String
+alphanumericWords =
+    custom
+        (Random.int 1 10
+            |> Random.andThen (\i -> Random.list i alphanumericWord)
+            |> Random.map (String.join " ")
+        )
+        (Shrink.keepIf (String.any isAlphanumeric) Shrink.string)
+
+
+alphanumericWord : Random.Generator String
+alphanumericWord =
+    Random.int 1 10
+        |> Random.andThen (\i -> Random.list i alphanumericChar)
+        |> Random.map String.fromList
+
+
+alphanumericChar : Random.Generator Char
+alphanumericChar =
+    String.toList "0123456789abcdefghijklmnropqstuvwxyzABCDEFGHIJKLMNROPQSTUVWXYZ"
+        |> Random.sample
+        |> Random.map (Maybe.withDefault 'a')
+
+
+isAlphanumeric : Char -> Bool
+isAlphanumeric c =
+    (c >= '0') && (c <= '9') || (c >= 'a') && (c <= 'z')
